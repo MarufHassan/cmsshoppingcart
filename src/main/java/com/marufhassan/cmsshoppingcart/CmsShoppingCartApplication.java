55cb8b26bb6c0272bf5b62ff78b857ef0f088194
Maruf Hassan <marufxhassan@gmail.com>
2021-01-22 00:38:46 +0600

    Initial Commit

---
+++ src/main/java/com/marufhassan/cmsshoppingcart/CmsShoppingCartApplication.java
@@ -0,0 +1,12 @@
+package com.marufhassan.cmsshoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsShoppingCartApplication.class, args);
+	}
+}
---
+++ src/test/java/com/marufhassan/cmsshoppingcart/CmsshoppingcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marufhassan.cmsshoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsshoppingcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
