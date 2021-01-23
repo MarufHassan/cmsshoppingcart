2f1b03a3494d62aae1d86aefbeffabe91f46350a
Maruf Hassan <marufxhassan@gmail.com>
2021-01-23 13:41:29 +0600

    GET edit category

--- src/main/java/com/marufhassan/cmsshoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -6,13 +6,13 @@ import javax.validation.Valid;
 
 import com.marufhassan.cmsshoppingcart.models.CategoryRepository;
 import com.marufhassan.cmsshoppingcart.models.data.Category;
-import com.marufhassan.cmsshoppingcart.models.data.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -56,5 +56,12 @@ public class AdminCategoriesController {
             categoryRepo.save(category);
         }
         return "redirect:/admin/categories/add";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable int id, Model model) {
+        Category category = categoryRepo.getOne(id);
+        model.addAttribute("category", category);
+        return "/admin/categories/edit";
     }
 }
