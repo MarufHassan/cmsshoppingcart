16c81c0098917c1748bca98cefe540e80f9f8244
Maruf Hassan <marufxhassan@gmail.com>
2021-01-23 13:12:19 +0600

    GET add category

--- src/main/java/com/marufhassan/cmsshoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -23,4 +23,9 @@ public class AdminCategoriesController {
         model.addAttribute("categories", categories);
         return "admin/categories/index";
     }
+
+    @GetMapping("/add")
+    public String add(Category category) {
+        return "admin/categories/add";
+    }
 }
