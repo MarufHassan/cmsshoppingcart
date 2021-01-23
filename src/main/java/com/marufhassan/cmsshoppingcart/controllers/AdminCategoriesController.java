fda874e67e01647300ef9b6ac978e0154b9699e8
Maruf Hassan <marufxhassan@gmail.com>
2021-01-23 13:23:08 +0600

    POST add category

--- src/main/java/com/marufhassan/cmsshoppingcart/controllers/AdminCategoriesController.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/controllers/AdminCategoriesController.java
@@ -2,14 +2,20 @@ package com.marufhassan.cmsshoppingcart.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.marufhassan.cmsshoppingcart.models.CategoryRepository;
 import com.marufhassan.cmsshoppingcart.models.data.Category;
+import com.marufhassan.cmsshoppingcart.models.data.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -27,5 +33,28 @@ public class AdminCategoriesController {
     @GetMapping("/add")
     public String add(Category category) {
         return "admin/categories/add";
+    }
+
+    @PostMapping("/add")
+    public String add(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "admin/categories/add";
+        }
+        redirectAttributes.addFlashAttribute("message", "Category added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+        
+        String slug = category.getName().toLowerCase().replace(" ", "-");
+        Category categoryExists = categoryRepo.findByName(category.getName());
+
+        if (categoryExists != null) {
+            redirectAttributes.addFlashAttribute("message", "Category exists, choose another");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("categoryInfo", category);
+        } else {
+            category.setSlug(slug);
+            category.setSorting(100);
+            categoryRepo.save(category);
+        }
+        return "redirect:/admin/categories/add";
     }
 }
--- src/main/java/com/marufhassan/cmsshoppingcart/models/CategoryRepository.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/models/CategoryRepository.java
@@ -5,5 +5,5 @@ import com.marufhassan.cmsshoppingcart.models.data.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    
+    Category findByName(String name);
 }
