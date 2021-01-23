3f84691ea45c955357a887599cbf557c854d74cd
Maruf Hassan <marufxhassan@gmail.com>
2021-01-23 12:49:27 +0600

    Categories table, entity and repository

---
+++ src/main/java/com/marufhassan/cmsshoppingcart/models/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.marufhassan.cmsshoppingcart.models;
+
+import com.marufhassan.cmsshoppingcart.models.data.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    
+}
---
+++ src/main/java/com/marufhassan/cmsshoppingcart/models/data/Category.java
@@ -0,0 +1,25 @@
+package com.marufhassan.cmsshoppingcart.models.data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "categories")
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(min = 2, message = "Name must be at least 2 characters long")
+    private String name;
+    
+    private String slug;
+    private int sorting;
+}
