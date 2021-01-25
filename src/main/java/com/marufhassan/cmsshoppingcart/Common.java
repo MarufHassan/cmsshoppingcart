4587680eb2fb9963f286221d6bbbfb0a01108f30
Maruf Hassan <marufxhassan@gmail.com>
2021-01-25 22:16:39 +0600

    Cart partial

--- src/main/java/com/marufhassan/cmsshoppingcart/Common.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/Common.java
@@ -1,7 +1,11 @@
 package com.marufhassan.cmsshoppingcart;
 
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import com.marufhassan.cmsshoppingcart.models.Cart;
 import com.marufhassan.cmsshoppingcart.models.CategoryRepository;
 import com.marufhassan.cmsshoppingcart.models.PageRepository;
 import com.marufhassan.cmsshoppingcart.models.data.Category;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
+@SuppressWarnings("unchecked")
 public class Common {
     @Autowired
     private PageRepository pageRepo;
@@ -21,11 +26,29 @@ public class Common {
     private CategoryRepository categoryRepo;
 
     @ModelAttribute
-    public void sharedData(Model model) {
+    public void sharedData(Model model, HttpSession session) {
         List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
         List<Category> categories = categoryRepo.findAll();
 
+        boolean cartActive = false;
+        if (session.getAttribute("cart") != null) {
+            Map<Integer, Cart> cart = (Map<Integer, Cart>) session.getAttribute("cart");
+            int size = 0;
+            double total = 0;
+
+            for (Cart value : cart.values()) {
+                size += value.getQuantity();
+                total += value.getQuantity() * Double.parseDouble(value.getPrice());
+            }
+
+            model.addAttribute("csize", size);
+            model.addAttribute("ctotal", total);
+
+            cartActive = true;
+        }
+
         model.addAttribute("cpages", pages);
         model.addAttribute("ccategories", categories);
+        model.addAttribute("cartActive", cartActive);
     }
 }
