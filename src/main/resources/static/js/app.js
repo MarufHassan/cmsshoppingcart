61b6ed6b993715c1cc1e29e63ad56acbf6c107e8
Maruf Hassan <marufxhassan@gmail.com>
2021-01-25 17:27:52 +0600

    Adding a WYSIWIG

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,6 +2,20 @@ $(function() {
     $("a.confirmDeletion").click(function() {
         if (!confirm("Confirm Deletion")) return false;
     });
+    if ($("#content").length) {
+        ClassicEditor
+            .create(document.querySelector("#content"))
+            .catch(error => {
+                console.log(error);
+            });
+    }
+    if ($("#description").length) {
+        ClassicEditor
+            .create(document.querySelector("#description"))
+            .catch(error => {
+                console.log(error);
+            });
+    }
 });
 
 function readURL(input, idNum) {
