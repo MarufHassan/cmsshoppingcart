7bd570a2e49c3c7aafd5bf48113d5276e0dbb606
Maruf Hassan <marufxhassan@gmail.com>
2021-01-25 20:05:03 +0600

    Categories menu in pages

--- src/main/java/com/marufhassan/cmsshoppingcart/Common.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/Common.java
@@ -2,7 +2,9 @@ package com.marufhassan.cmsshoppingcart;
 
 import java.util.List;
 
+import com.marufhassan.cmsshoppingcart.models.CategoryRepository;
 import com.marufhassan.cmsshoppingcart.models.PageRepository;
+import com.marufhassan.cmsshoppingcart.models.data.Category;
 import com.marufhassan.cmsshoppingcart.models.data.Page;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,15 @@ public class Common {
     @Autowired
     private PageRepository pageRepo;
 
+    @Autowired
+    private CategoryRepository categoryRepo;
+
     @ModelAttribute
     public void sharedData(Model model) {
         List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
-        model.addAttribute("cpages", pages);
+        List<Category> categories = categoryRepo.findAll();
 
+        model.addAttribute("cpages", pages);
+        model.addAttribute("ccategories", categories);
     }
 }
