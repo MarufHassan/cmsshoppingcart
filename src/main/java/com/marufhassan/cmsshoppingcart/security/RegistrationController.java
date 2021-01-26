bee6132cdf33acea752a1f1577ce58d2efee5e2f
Maruf Hassan <marufxhassan@gmail.com>
2021-01-26 16:35:18 +0600

    POST register

--- src/main/java/com/marufhassan/cmsshoppingcart/security/RegistrationController.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/security/RegistrationController.java
@@ -1,12 +1,17 @@
 package com.marufhassan.cmsshoppingcart.security;
 
+import javax.validation.Valid;
+
 import com.marufhassan.cmsshoppingcart.models.UserRepository;
 import com.marufhassan.cmsshoppingcart.models.data.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +26,22 @@ public class RegistrationController {
     @GetMapping
     public String register(User user) {
         return "register";
+    }
+
+    @PostMapping
+    public String register(@Valid User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+
+        if (!user.getPassword().equals(user.getConfirmPassword())) {
+            model.addAttribute("passwordMatchProblem", "Password do not match");
+            return "register";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepo.save(user);
+
+        return "redirect:/login";
     }
 }
