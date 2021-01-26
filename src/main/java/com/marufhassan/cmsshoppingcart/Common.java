e3a0ff7da0801905cc8feb2c7fb337a3fcbf260d
Maruf Hassan <marufxhassan@gmail.com>
2021-01-27 00:39:54 +0600

    csrf ajax post fix

--- src/main/java/com/marufhassan/cmsshoppingcart/Common.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/Common.java
@@ -33,7 +33,7 @@ public class Common {
         }
 
         List<Page> pages = pageRepo.findAllByOrderBySortingAsc();
-        List<Category> categories = categoryRepo.findAll();
+        List<Category> categories = categoryRepo.findAllByOrderBySortingAsc();
 
         boolean cartActive = false;
         if (session.getAttribute("cart") != null) {
